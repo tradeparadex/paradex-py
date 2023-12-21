f122cf2efb19a3a2f3b3e80b31e4b96b378d00ec
Amay Shah <amayvs@gmail.com>
2023-12-21 13:51:08 +0530

    Minor cleanup

--- paradex_py/account/utils.py
+++ paradex_py/account/utils.py
@@ -2,20 +2,23 @@ import hashlib
 
 from starknet_py.common import int_from_hex  # type: ignore[import-untyped]
 
+SHA256_EC_MAX_DIGEST = 2**256
+
+
+def _padded_hex(x: int) -> str:
+    # Hex string should have an even
+    # number of characters to convert to bytes.
+    hex_str = hex(x)[2:]
+    return hex_str if len(hex_str) % 2 == 0 else "0" + hex_str
+
 
 def _indexed_sha256(seed: int, index: int) -> int:
-    def padded_hex(x: int) -> str:
-        # Hex string should have an even
-        # number of characters to convert to bytes.
-        hex_str = hex(x)[2:]
-        return hex_str if len(hex_str) % 2 == 0 else "0" + hex_str
-
-    digest = hashlib.sha256(bytes.fromhex(padded_hex(seed) + padded_hex(index))).hexdigest()
+    digest = hashlib.sha256(bytes.fromhex(_padded_hex(seed) + _padded_hex(index))).hexdigest()
     return int_from_hex(digest)
 
 
 def grind_key(key_seed: int, key_value_limit: int) -> int:
-    max_allowed_value = 2**256 - (2**256 % key_value_limit)
+    max_allowed_value = SHA256_EC_MAX_DIGEST - (SHA256_EC_MAX_DIGEST % key_value_limit)
     current_index = 0
 
     key = _indexed_sha256(seed=key_seed, index=current_index)
