da5efea503d81a42e1625f4739ce1f94c557dd7e
Amay Shah <amayvs@gmail.com>
2024-02-07 01:13:33 +0530

    Add starknet account with rust signing (#7)

--- paradex_py/account/utils.py
+++ paradex_py/account/utils.py
@@ -1,8 +1,19 @@
+import functools
 import hashlib
+from typing import List, Sequence
 
+from eth_account.messages import encode_typed_data
+from starknet_crypto_py import get_public_key as rs_get_public_key
+from starknet_crypto_py import pedersen_hash as rs_pedersen_hash
+from starknet_crypto_py import sign as rs_sign
+from starknet_crypto_py import verify as rs_verify
 from starknet_py.common import int_from_hex
+from starknet_py.constants import EC_ORDER
+from starknet_py.net.models.typed_data import TypedData
+from web3.auto import w3
 
 SHA256_EC_MAX_DIGEST = 2**256
+DEFAULT_K = 32
 
 
 def _padded_hex(x: int) -> str:
@@ -17,7 +28,7 @@ def _indexed_sha256(seed: int, index: int) -> int:
     return int_from_hex(digest)
 
 
-def grind_key(key_seed: int, key_value_limit: int) -> int:
+def _grind_key(key_seed: int, key_value_limit: int) -> int:
     max_allowed_value = SHA256_EC_MAX_DIGEST - (SHA256_EC_MAX_DIGEST % key_value_limit)
     current_index = 0
 
@@ -27,3 +38,74 @@ def grind_key(key_seed: int, key_value_limit: int) -> int:
         key = _indexed_sha256(seed=key_seed, index=current_index)
 
     return key % key_value_limit
+
+
+def _sign_stark_key_message(stark_key_message, eth_private_key: int) -> str:
+    encoded = encode_typed_data(full_message=stark_key_message)
+    signed = w3.eth.account.sign_message(encoded, eth_private_key)
+    return signed.signature.hex()
+
+
+def _get_private_key_from_eth_signature(eth_signature_hex: str) -> int:
+    r = eth_signature_hex[2 : 64 + 2]
+    return _grind_key(int_from_hex(r), EC_ORDER)
+
+
+def derive_stark_key(eth_private_key: int, stark_key_msg: TypedData) -> int:
+    message_signature = _sign_stark_key_message(stark_key_msg, eth_private_key)
+    private_key = _get_private_key_from_eth_signature(message_signature)
+    return private_key
+
+
+def flatten_signature(sig: List[int]) -> str:
+    return f'["{sig[0]}","{sig[1]}"]'
+
+
+# ###
+# Override functions in starknet_py.hash.utils that use cpp
+# to use the starknet_crypto_py library
+# ###
+
+
+def private_to_stark_key(priv_key: int) -> int:
+    """
+    Deduces the public key given a private key.
+    """
+    return rs_get_public_key(priv_key)
+
+
+def pedersen_hash(left: int, right: int) -> int:
+    """
+    One of two hash functions (along with _starknet_keccak) used throughout Starknet.
+    """
+    # return cpp_hash(left, right)
+    return rs_pedersen_hash(left, right)
+
+
+def compute_hash_on_elements(data: Sequence) -> int:
+    """
+    Computes a hash chain over the data, in the following order:
+        h(h(h(h(0, data[0]), data[1]), ...), data[n-1]), n).
+
+    The hash is initialized with 0 and ends with the data length appended.
+    The length is appended in order to avoid collisions of the following kind:
+    H([x,y,z]) = h(h(x,y),z) = H([w, z]) where w = h(x,y).
+    """
+    return functools.reduce(pedersen_hash, [*data, len(data)], 0)
+
+
+def message_signature(msg_hash: int, priv_key: int) -> tuple[int, int]:
+    """
+    Signs the message with private key.
+    """
+    return rs_sign(private_key=priv_key, msg_hash=msg_hash, k=DEFAULT_K)
+
+
+def verify_message_signature(msg_hash: int, signature: List[int], public_key: int) -> bool:
+    """
+    Verifies ECDSA signature of a given message hash with a given public key.
+    Returns true if public_key signs the message.
+    """
+    sig_r, sig_s = signature
+    sig_w = pow(sig_s, -1, EC_ORDER)
+    return rs_verify(msg_hash=msg_hash, r=sig_r, s=sig_w, public_key=public_key)
--- tests/api/test_account.py
+++ tests/api/test_account.py
@@ -1,17 +1,83 @@
 import pytest
+from starknet_py.common import int_from_hex
 
 from paradex_py.account.account import ParadexAccount
-from paradex_py.api.api_client import ApiClient
+from tests.mocks.api_client import MockApiClient
 
 
 @pytest.mark.asyncio
-async def test_account():
-    api_client = ApiClient(env="testnet")
+async def test_account_l1_private_key():
+    api_client = MockApiClient()
     config = await api_client.load_system_config()
 
-    eth_private_key = "f8e4d1d772cdd44e5e77615ad11cc071c94e4c06dc21150d903f28e6aa6abdff"
-    account = ParadexAccount(config, eth_private_key)
+    l1_private_key = int_from_hex("f8e4d1d772cdd44e5e77615ad11cc071c94e4c06dc21150d903f28e6aa6abdff")
+    account_address = int_from_hex("0x129c135ed63df9353885e292be4426b8ed6122b13c6c0e1bb787288a1f5adfa")
 
-    assert account.address == "0x129c135ed63df9353885e292be4426b8ed6122b13c6c0e1bb787288a1f5adfa"
-    assert account.private_key == "0x543b6cf6c91817a87174aaea4fb370ac1c694e864d7740d728f8344d53e815"
-    assert account.public_key == "0x2c144d2f2d4fc61b6f8967f3ba0012a87d90140bcfe5a3e92e8df83258c960f"
+    account = ParadexAccount(
+        config=config,
+        l1_private_key=l1_private_key,
+    )
+
+    assert account.address == account_address
+    assert account.starknet.address == account_address
+
+    assert account.private_key == int_from_hex("0x543b6cf6c91817a87174aaea4fb370ac1c694e864d7740d728f8344d53e815")
+    assert account.public_key == int_from_hex("0x2c144d2f2d4fc61b6f8967f3ba0012a87d90140bcfe5a3e92e8df83258c960f")
+
+
+@pytest.mark.asyncio
+async def test_account_private_key():
+    api_client = MockApiClient()
+    config = await api_client.load_system_config()
+
+    private_key = int_from_hex("0x543b6cf6c91817a87174aaea4fb370ac1c694e864d7740d728f8344d53e815")
+    account_address = int_from_hex("0x129c135ed63df9353885e292be4426b8ed6122b13c6c0e1bb787288a1f5adfa")
+
+    account = ParadexAccount(
+        config=config,
+        private_key=private_key,
+    )
+
+    assert account.address == account_address
+    assert account.starknet.address == account_address
+
+    assert account.private_key == private_key
+    assert account.public_key == int_from_hex("0x2c144d2f2d4fc61b6f8967f3ba0012a87d90140bcfe5a3e92e8df83258c960f")
+
+
+@pytest.mark.asyncio
+async def test_account_onboarding_signature():
+    api_client = MockApiClient()
+    config = await api_client.load_system_config()
+
+    private_key = int_from_hex("0x543b6cf6c91817a87174aaea4fb370ac1c694e864d7740d728f8344d53e815")
+    account = ParadexAccount(
+        config=config,
+        private_key=private_key,
+    )
+
+    sig = account.onboarding_signature()
+    assert (
+        sig
+        == '["1977703130303461992863803129734853218488251484396280000763960303272760326570","2304839285473787246223990153934072220920935645937627960319990072742739460432"]'
+    )
+
+
+@pytest.mark.asyncio
+async def test_account_auth_signature():
+    api_client = MockApiClient()
+    config = await api_client.load_system_config()
+
+    private_key = int_from_hex("0x543b6cf6c91817a87174aaea4fb370ac1c694e864d7740d728f8344d53e815")
+    account = ParadexAccount(
+        config=config,
+        private_key=private_key,
+    )
+
+    timestamp = 1706868900
+    expiry = 1706955300
+    sig = account.auth_signature(timestamp, expiry)
+    assert (
+        sig
+        == '["1977703130303461992863803129734853218488251484396280000763960303272760326570","2372141293109856884078160483922619445669904377109121850462217324440649578224"]'
+    )
