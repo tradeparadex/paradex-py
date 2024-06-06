2dbbcece5e0d1b1a7583e1e5817ecdc0a5f1b4c6
Amay Shah <amayvs@gmail.com>
2024-06-07 00:12:40 +1000

    Support stop limit and market orders

--- paradex_py/common/order.py
+++ paradex_py/common/order.py
@@ -16,6 +16,8 @@ class OrderAction(Enum):
 class OrderType(Enum):
     Market = "MARKET"
     Limit = "LIMIT"
+    StopLimit = "STOP_LIMIT"
+    StopMarket = "STOP_MARKET"
 
 
 class OrderStatus(Enum):
@@ -51,6 +53,11 @@ class Order:
         signature_timestamp: Optional[int] = None,
         instruction: str = "GTC",
         reduce_only: bool = False,
+        recv_window: Optional[int] = None,
+        stp: Optional[
+            str
+        ] = None,  # Self Trade Prevention, EXPIRE_MAKER, EXPIRE_TAKER or EXPIRE_BOTH, default: EXPIRE_TAKER
+        trigger_price: Optional[Decimal] = None,
     ) -> None:
         ts = time_now_milli_secs()
         self.id: str = ""
@@ -65,14 +72,16 @@ class Order:
         self.client_id = client_id
         self.instruction = instruction
         self.reduce_only = reduce_only
-        # created_at is in milliseconds
-        self.created_at = ts
+        self.created_at = ts  # milliseconds
         self.cancel_reason = ""
         self.last_action = OrderAction.NAN
         self.last_action_time = 0
         self.cancel_attempts = 0
         self.signature = ""
         self.signature_timestamp = ts if signature_timestamp is None else signature_timestamp
+        self.recv_window = recv_window
+        self.stp = stp
+        self.trigger_price = trigger_price
 
     def __repr__(self) -> str:
         ord_status = self.status.value
@@ -80,7 +89,9 @@ class Order:
             ord_status += f"({self.cancel_reason})"
         msg = f"{self.market} {ord_status} {self.order_type.name} "
         msg += f"{self.order_side} {self.remaining}/{self.size}"
-        msg += f"@{self.limit_price}" if self.order_type == OrderType.Limit else ""
+        msg += f"@{self.limit_price}" if self.is_limit_type() else ""
+        msg += f"trigger@{self.trigger_price}" if self.trigger_price else ""
+        msg += f"recv_window={self.recv_window}" if self.recv_window else ""
         msg += f";id={self.id}" if self.id else ""
         msg += f";client_id={self.client_id}" if self.client_id else ""
         msg += f";last_action:{self.last_action}" if self.last_action != OrderAction.NAN else ""
@@ -103,9 +114,13 @@ class Order:
             "instruction": self.instruction,
             "signature": self.signature,
             "signature_timestamp": self.signature_timestamp,
+            "recv_window": self.recv_window,
+            "stp": self.stp,
         }
-        if self.order_type == OrderType.Limit:
+        if self.is_limit_type():
             order_dict["price"] = str(self.limit_price)
+        if self.trigger_price:
+            order_dict["trigger_price"] = str(self.trigger_price)
         if self.reduce_only:
             order_dict["flags"] = ["REDUCE_ONLY"]
         return order_dict
@@ -117,3 +132,6 @@ class Order:
 
     def chain_size(self) -> str:
         return str(int(self.size.scaleb(8)))
+
+    def is_limit_type(self) -> bool:
+        return self.order_type == OrderType.Limit or self.order_type == OrderType.StopLimit
