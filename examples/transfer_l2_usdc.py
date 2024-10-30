3a7603cd11c5eaef832f9c59459332ed3ef178e3
Linlu　 <ma.linlu.alina@gmail.com>
2024-10-30 11:07:00 +0800

    add transfer on l2 to sdk (#50)
    * add transfer on l2 to sdk
    
    * use transfer function
    
    * simplify
    
    * update doc and bump version
    
    * clean up

---
+++ examples/transfer_l2_usdc.py
@@ -0,0 +1,31 @@
+import asyncio
+import os
+from decimal import Decimal
+
+from starknet_py.common import int_from_hex
+
+from paradex_py import Paradex
+from paradex_py.environment import TESTNET
+
+# Environment variables
+TEST_L1_ADDRESS = os.getenv("L1_ADDRESS", "")
+TEST_L1_PRIVATE_KEY = int_from_hex(os.getenv("L1_PRIVATE_KEY", ""))
+LOG_FILE = os.getenv("LOG_FILE", "FALSE").lower() == "true"
+
+if LOG_FILE:
+    from paradex_py.common.file_logging import file_logger
+
+    logger = file_logger
+else:
+    from paradex_py.common.console_logging import console_logger
+
+    logger = console_logger
+
+
+paradex = Paradex(env=TESTNET, l1_address=TEST_L1_ADDRESS, l1_private_key=TEST_L1_PRIVATE_KEY)
+
+receipient_address = ""
+amount = Decimal(0)
+logger.info(f"Transferring USDC from {TEST_L1_ADDRESS} to {receipient_address} amount {amount}")
+
+asyncio.run(paradex.account.transfer_on_l2(receipient_address, amount))
--- paradex_py/account/account.py
+++ paradex_py/account/account.py
@@ -1,4 +1,6 @@
+import logging
 import time
+from decimal import Decimal
 from enum import IntEnum
 from typing import Optional
 
@@ -136,3 +138,45 @@ class ParadexAccount:
     def sign_order(self, order: Order) -> str:
         sig = self.starknet.sign_message(build_order_message(self.l2_chain_id, order))
         return flatten_signature(sig)
+
+    async def transfer_on_l2(self, target_l2_address: str, amount_decimal: Decimal):
+        try:
+            # Load contracts
+            paraclear_address = int_from_hex(self.config.paraclear_address)
+            usdc_address = int_from_hex(self.config.bridged_tokens[0].l2_token_address)
+            paraclear_contract = await self.starknet.load_contract(paraclear_address)
+            account_contract = await self.starknet.load_contract(self.l2_address)
+
+            paraclear_decimals = self.config.paraclear_decimals
+
+            # Get token asset balance
+            token_asset_balance = await paraclear_contract.functions["getTokenAssetBalance"].call(
+                account=self.l2_address, token_address=usdc_address
+            )
+            logging.info(f"USDC balance on Paraclear: {token_asset_balance.balance / 10**paraclear_decimals}")
+
+            # Calculate amounts
+            amount_paraclear = int(amount_decimal * 10**paraclear_decimals)
+            logging.info(f"Amount to transfer to {target_l2_address}: {amount_paraclear}")
+
+            # Prepare calls
+            calls = [
+                paraclear_contract.functions["transfer"].prepare_invoke_v1(
+                    recipient=int_from_hex(target_l2_address),
+                    token_address=usdc_address,
+                    amount=amount_paraclear,
+                ),
+            ]
+
+            # Check if multisig is required
+            need_multisig = await self.starknet.check_multisig_required(account_contract)
+
+            # Prepare and send transaction
+            func_name = "transferOnL2"
+            prepared_invoke = await self.starknet.prepare_invoke(calls=calls)
+            await self.starknet.process_invoke(account_contract, need_multisig, prepared_invoke, func_name)
+
+        except Exception as e:
+            logging.error(f"Error during transfer_on_l2: {e}")
+            # Re-raise the exception to handle it upstream if necessary
+            raise
