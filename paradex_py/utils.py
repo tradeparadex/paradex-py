e5ea02aa37d11984accfbd62f0462438fbfb784c
Mattia D'Antonio <m.dantonio81@gmail.com>
2025-11-27 15:55:28 +0800

    Support RPC v0_9

--- paradex_py/utils.py
+++ paradex_py/utils.py
@@ -1,32 +1,4 @@
-import secrets
 import time
-
-from starknet_py.net.client_models import ResourceBounds, ResourceBoundsMapping
-
-l1_gas_max_amount = 10000000
-l1_gas_max_price_per_unit = 10000000000
-l1_data_gas_max_amount = 10000000
-l1_data_gas_max_price_per_unit = 10000000000
-l2_gas_max_amount = 100000000
-l2_gas_max_price_per_unit = 10000000000
-
-
-def random_resource_bounds(randomized_factor: int = 100000) -> ResourceBoundsMapping:
-    randomized = secrets.randbelow(randomized_factor)
-    return ResourceBoundsMapping(
-        l1_gas=ResourceBounds(
-            max_amount=l1_gas_max_amount + randomized,
-            max_price_per_unit=l1_gas_max_price_per_unit,
-        ),
-        l1_data_gas=ResourceBounds(
-            max_amount=l1_data_gas_max_amount + randomized,
-            max_price_per_unit=l1_data_gas_max_price_per_unit,
-        ),
-        l2_gas=ResourceBounds(
-            max_amount=l2_gas_max_amount + randomized,
-            max_price_per_unit=l2_gas_max_price_per_unit,
-        ),
-    )
 
 
 def time_now_milli_secs() -> int:
