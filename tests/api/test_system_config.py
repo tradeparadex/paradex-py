f8c97f72d91340cd61ab9929bf68aa98c3df3670
Sergey Vidyuk <svidyuk@gmail.com>
2026-01-30 12:53:24 +0800

    Fix test_system_config: starknet_gateway_url is now empty (#123)
    * Bump version to 0.5.5-rc2
    
    * Fix test_system_config: starknet_gateway_url is now empty

--- tests/api/test_system_config.py
+++ tests/api/test_system_config.py
@@ -4,7 +4,7 @@ from paradex_py.environment import TESTNET
 
 def test_system_config():
     paradex = Paradex(env=TESTNET)
-    assert paradex.config.starknet_gateway_url == "https://potc-testnet-sepolia.starknet.io"
+    assert paradex.config.starknet_gateway_url == ""
     assert paradex.config.starknet_chain_id == "PRIVATE_SN_POTC_SEPOLIA"
     assert paradex.config.block_explorer_url == "https://app.testnet.paradex.trade/explorer"
     assert paradex.config.bridged_tokens[0].name == "TEST USDC"
